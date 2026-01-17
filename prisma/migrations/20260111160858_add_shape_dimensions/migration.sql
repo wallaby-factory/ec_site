a881fad7e589bb1357e5027e24d5cc5e66028b28
wallaby-factory <monstrous.monstro.12@gmail.com>
2026-01-17 23:45:04 +0900

    Remove sqlite migrations to fix postgres deployment

--- prisma/migrations/20260111160858_add_shape_dimensions/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "OrderItem" ADD COLUMN "depth" INTEGER;
-ALTER TABLE "OrderItem" ADD COLUMN "diameter" INTEGER;
-
--- AlterTable
-ALTER TABLE "PublicOrder" ADD COLUMN "depth" INTEGER;
-ALTER TABLE "PublicOrder" ADD COLUMN "diameter" INTEGER;
