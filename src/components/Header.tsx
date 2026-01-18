e854ce2fe1de49d923bf4cd38773dae1f6b57d2e
wallaby-factory <monstrous.monstro.12@gmail.com>
2026-01-18 10:03:58 +0900

    Fix duplicate imports in Header again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 
-import Link from "next/link";
-import { getCurrentUser } from "@/actions/auth";
-import { LogoutButton } from "./LogoutButton";
+
 import Link from "next/link";
 import { getCurrentUser } from "@/actions/auth";
 import { LogoutButton } from "./LogoutButton";
